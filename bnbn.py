ae81d94e7c0d885e1303f424714d5cbfc5d704d4
SeanMelant <colgatetoothpasteyum@gmail.com>
2014-12-09 14:11:47 -0600

    ddfd
    fdffgdffdgdfdg

---
+++ bnbn.py
@@ -0,0 +1,209 @@
+print "A game by Sean and Eli"
+print "______________________"
+print "the revenge of the dogg"
+print "______________________"
+print "snoop: ...."
+print "--press a during dialouge to continue--"
+userinput = (raw_input())
+if  userinput == "a":
+    print "snoop: that man.....he killed me..."
+    userinput = (raw_input())
+if  userinput == "a":
+    print "i MUST EXTRACT MUH REVERNGEE......HANK?!??! DID YOU BRING ME BACK???!!!"
+    userinput = (raw_input())
+    if userinput == "a":
+        print "Hank: bobbeh, it's time you risen like hey-zues crust!"
+        userinput = (raw_input())
+    if userinput == "a":
+        print "snoop: i hate to say it, but yo bobby be gone like elvis's dong"
+        userinput = (raw_input())
+    if userinput == "a":
+        print "Hank: dammit, snoopy doopy do, where the hell are you? we got some doodoo to doo meow"
+        userinput = (raw_input())
+    if  userinput == "a":
+        print "what does snoop do? 1 to kill Hank, 2 to join him on his quest for coca- i mean propaine"
+        userinput = (raw_input())
+    if userinput == "1":
+        print "snoop: sorry hill, its time to get down and doggy"
+        userinput = (raw_input())
+        if "snoop: sorry hill, its time to get down and doggy":
+                print "Ok, bobbeh! time to pheal mai wraph!"
+                userinput = (raw_input())
+                snoophealth = 420
+                hankhealth = 101
+                weedattack = 15
+                blunthrow = 50
+                therappasnappa = 5
+                propanepooter = 37
+                imnoturban = 69
+                while hankhealth > 0 and snoophealth > 0:
+                    print "press 1 for weed attack, 2 for blunt throw, or 3 for rapper snapper"
+                    userinput = (raw_input())
+                    if userinput == "1":
+                        hankhealth = hankhealth - weedattack
+                        print "hank lost 15 health"
+                        print "hank used an attack"
+                        snoophealth = snoophealth - (propanepooter or imnoturban)
+                        print "snoopy doopy" + str(snoophealth)
+                        print "Hankle Hillol" + str(hankhealth)
+                    if userinput == "2":
+                        hankhealth = hankhealth - blunthrow
+                        snoophealth = snoophealth - (blunthrow * 4)
+                        print "hank lost 50, but you lost 200 :OOOOOOOOOOOO"
+                        print "hank used an attack"
+                        snoophealth = snoophealth - (propanepooter or imnoturban)
+                        print "snoopy doopy" + str(snoophealth)
+                        print "Hankle Hillol" + str(hankhealth)
+                    if userinput == "3":
+                        hankhealth = hankhealth - therappasnappa
+                        print "hank loses" + str(therappasnappa)
+                        therappasnappa = therappasnappa + int(1)
+                        print "hank used an attack"
+                        snoophealth = snoophealth - (propanepooter or imnoturban)
+                        print "Snoopy Doopy" + str(snoophealth)
+                        print "Hankle Hillol" + str(hankhealth)
+                    if hankhealth <= 0 and snoophealth > 0:
+                        print "hank: I'M COMMIN BOBBEH"
+                        userinput = (raw_input())
+                    if snoophealth <= 0:
+                        print "you are dead, your family is dead, your friends are dead, your pets have been skinned alive, everyone you know is a junkie, your moms a slut, your going to hell, deal with it, GAME OVER"
+                        userinput = (raw_input())
+                    if userinput== "a":
+                        print "snoop dogg: i find your lack of green disturbing"
+                        userinput = (raw_input())
+                    if userinput == "a":
+                        print "Lv up yo!"
+                        userinput = (raw_input())
+                    if userinput== "a":
+                        print "jontron: nooooooo hannnkkkkkkkkkkkkkkkkk"
+                        userinput = (raw_input())
+                    if userinput== "a":
+                        print "snoop: jon... you ordered me dead. you killed me dead"
+                        userinput = (raw_input())
+                    if userinput == "a":
+                        print "jontron: I'M SOOREH! PLS SPARCE ME SNOOPY DOOPY!"
+                        userinput == (raw_input())
+                    if userinput == "a":
+                        print "snoop: egh...shall i sparce this uno?"
+                        userinput = (raw_input())
+                    if userinput == "a":
+                        print "press 1 to spare jontron, and 2 to stab him like he's baloneighh"
+                        userinput = (raw_input())
+                        if userinput == "1" and "press 1 to spare jontron, and 2 to stab him like he's baloneighh":
+                            print "snoop: ...... you are......forgiven"
+                            userinput = (raw_input())
+                        if userinput == "a":
+                            print "jontron: I....i thank you...snoop sir, i will erase alluf yer criminal records. Farewell snoop, my friend"
+                            userinput = (raw_input())
+                        if userinput == "a":
+                            print "ENDING UNLOCKED: FORGIVENESS (THE BEST HO)"
+                        if userinput == "2" and "press 1 to spare jontron, and 2 to stab him like he's baloneighh":
+                            print "snoop: its tiem to kill da jantran!!!!!!!"
+                            userinput = (raw_input())
+                        if userinput == "a":
+                            print "JONNY BOY: bring it on! it's time i killed what i hammed what i started!"
+                            userinput = (raw_input())
+                            snoophealth = 481.5
+                            jonnyhalth = 212
+                            weedattack = 20
+                            blunthrow = 55
+                            therappasnappa = 10
+                            hathrow = 42.0
+                            ham = 10
+                            while jonnyhalth > 0 and snoophealth > 0:
+                                print "press 1 for weed attack, 2 for blunt throw, or 3 for rapper snapper"
+                                userinput = (raw_input())
+                                if userinput == "1":
+                                        jonnyhalth = jonnyhalth - weedattack
+                                        print "jon lost 15 health"
+                                        print "jon used an attack"
+                                        snoophealth = snoophealth - (hathrow or ham)
+                                        print "snoopy doopy" + str(snoophealth)
+                                        print "jantran" + str(jonnyhalth)
+                                if userinput == "2":
+                                        jonnyhalth = jonnyhalth - blunthrow
+                                        snoophealth = snoophealth - (blunthrow * 4)
+                                        print "jon lost 50, but you lost 200 :OOOOOOOOOOOO"
+                                        print "jonnnnjnjnjn used an attack"
+                                        snoophealth = snoophealth - (hathrow or ham)
+                                        print "snoopy doopy" + str(snoophealth)
+                                        print "jonny" + str(jonnyhalth)
+                                if userinput == "3":
+                                        jonnyhalth = jonnyhalth - therappasnappa
+                                        print "jan loses" + str(therappasnappa)
+                                        therappasnappa = therappasnappa + int(1)
+                                        print "jun used an attack"
+                                        snoophealth = snoophealth - (hathrow or ham)
+                                        print "Snoopy Doopy" + str(snoophealth)
+                                        print "Janny" + str(jonnyhalth)
+                                if jonnyhalth <= 0 and snoophealth > 0:
+                                        print "jontron: I'm COMMIN JAquesleleleleleleelel! EEEEEEEEEEEEEEHHHHHHHHHHHHHHHHHHHHHHHHHH!"
+                                        userinput = (raw_input())
+                                if userinput == "a":
+                                        print "snoop: .....farewell.............jon....."
+                                        userinput = (raw_input())
+                                if userinput == "a":
+                                        print "ENDING ACHIVED: REVENGE"
+                                        print "THE ENDLEL"
+                                if snoophealth <= 0:
+                                        print "omg you lost to a fat guy lsoerrrrrrrrrrrrrrrrrr"
+    if userinput == "2" and "what does snoop do? 1 to kill Hank, 2 to join him on his quest for coca- i mean propaine":
+        print "snoop: ok bank bill, let's defeat evvil. It's on like a copywritten catchphrase!"
+        userinput = (raw_input())
+    if userinput == "a":
+        print "hank: oh look over there!! it's teali!"
+        userinput = (raw_input())
+    if userinput == "a":
+        print "Teali: Not so fwast!i is teali! teal eli! lolololoololol time to dye!!!!!!!!!!!!!!!!!"
+        userinput = (raw_input())
+    if userinput == "a":
+        print "snoopy doopy doo da: da fuq?"
+        userinput = (raw_input())
+        snoophealth = 420
+        TEALIHALTH = 200
+        weedattack = 20
+        blunthrow = 55
+        therappasnappa = 10
+        TealAttack = 5
+        TealinUp = 50
+        while TEALIHALTH > 0 and snoophealth > 0:
+            print "press 1 for weed attack, 2 for blunt throw, or 3 for rapper snapper"
+            userinput = (raw_input())
+            if userinput == "1":
+                    TEALIHALTH = TEALIHALTH - weedattack
+                    print "Teali lost 15 health"
+                    print "Teali: I'm tealin up! *Teali used an attack"
+                    snoophealth = snoophealth - (TealAttack or TealinUp)
+                    print "snoopy doopy" + str(snoophealth)
+                    print "TEALI" + str(TEALIHALTH)
+            if userinput == "2":
+                    TEALIHALTH = TEALIHALTH - blunthrow
+                    TEALIHALTH = TEALIHALTH - (blunthrow * 4)
+                    print "Teali lost 50, but you lost 200 :OOOOOOOOOOOO"
+                    print "Teali: I'm tealin up! *Teali used an attack"
+                    snoophealth = snoophealth - (TealAttack or TealinUp)
+                    print "snoopy doopy" + str(snoophealth)
+                    print "TEALI" + str(TEALIHALTH)
+            if userinput == "3":
+                    TEALIHALTH = TEALIHALTH - therappasnappa
+                    print "teali loses" + str(therappasnappa)
+                    therappasnappa = therappasnappa + int(1)
+                    print "Teali: I'm tealin up! *Teali used an attack"
+                    snoophealth = snoophealth - (TealAttack or TealinUp)
+                    print "Snoopy Doopy" + str(snoophealth)
+                    print "teal eli" + str(TEALIHALTH)
+            if TEALIHALTH <= 0 and snoophealth > 0:
+                    print "!!!!!!!!!!ERROR!!!!!!!!!!!!!"
+                    print "!!!!!!!!!!Reseting system!!!!!!!!!!!!!"
+                    userinput = (raw_input())
+            if userinput == "a":
+                print "JK LOL! teali died"
+                userinput = (raw_input())
+            if userinput == "a":
+                    print "snoop: .....farewell.............teali....."
+                    userinput = (raw_input())
+            if userinput == "a":
+                    print "ENDING ACHIVED: TEALIN' UP!"
+                    print "THE ENDLEL"
+            if snoophealth <= 0:
+                    print "Teali: Me and sean are going to take a dump on yo gravy!"
